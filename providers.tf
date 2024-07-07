9b193e84067615c4973319be812dbba69251be1a
AKSHAY2126 <142775801+AKSHAY2126@users.noreply.github.com>
2024-07-07 11:16:22 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -10,8 +10,8 @@ terraform {
 
 provider "azurerm" {
   features {}
-  tenant_id       = "a49be146-23d3-44fa-951f-f7210572c2ad"
-  client_id       = "24d083e5-cf7f-47c9-b923-17446ecc095b"
-  client_secret   = "0348Q~u76uOt66HUctvA2_hbR8VteOZc3hPd4aKq"
-  subscription_id = "71aaef51-8d4b-4b1d-9e60-7208f4010806"
+  tenant_id       = "a49be146-23d3-44fa-951f-f7210572c2ads"
+  client_id       = "24d083e5-cf7f-47c9-b923-17446ecc09c5b"
+  client_secret   = "0348Q~u76uOt66HUctvA2_hbR8VteOZc3hPd4aKfdq"
+  subscription_id = "71aaef51-8d4b-4b1d-9e60-7208f401078806"
 }
